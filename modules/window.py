7cc5b355d01f2622dfe93102270940b4dd5aa0f6
LenaFedchenko <lenafedcenko98@gmail.com>
2025-11-09 19:36:44 +0200

    added new btns

--- modules/window.py
+++ modules/window.py
@@ -1,5 +1,9 @@
 import PyQt6.QtWidgets as widgets
 from .app import app
+from PIL import Image
+# class for change from pil to qt
+from PIL.ImageQt import ImageQt
+import PyQt6.QtGui as gui
 
 main_window = widgets.QMainWindow()
 
@@ -40,3 +44,44 @@ main_frame_layout = widgets.QGridLayout()
 main_frame.setLayout(main_frame_layout)
 main_frame_layout.addWidget(first_frame, 1, 5)
 main_frame_layout.addWidget(second_frame, 1, 1)
+
+# created label
+label1 = widgets.QLabel(parent= first_frame, text="hello world")
+main_frame_layout.addWidget(label1)
+label1.setStyleSheet("font-size: 100px; color: pink;  font-weight:900")
+
+# load our picture
+image = Image.open("images/monkey.png")
+
+# changed picture for PQt6
+qt_image = ImageQt(image)
+label2 = widgets.QLabel(parent= second_frame)
+
+
+button = widgets.QPushButton(parent= main_frame, text= "push")
+button.setStyleSheet("background-color: pink; font-weight: 900;")
+
+
+def click():
+    print("aaaaa")
+
+
+button.clicked.connect(click)
+
+enter_text = widgets.QLineEdit(parent=main_frame)
+enter_text.setPlaceholderText("enter text")
+
+def func():
+    print(enter_text.text())
+
+enter_text.textChanged.connect(func)
+
+
+down_list = widgets.QComboBox(parent= main_frame)
+down_list.addItem("Afrika")
+down_list.addItem("Amerika")
+down_list.addItem(gui.QIcon("images/monkey.png"), "hello")
+down_list.currentTextChanged.connect(func)
+
+main_frame_layout.addWidget(button, 3, 2)
+main_frame_layout.addWidget(enter_text, 4, 4)
